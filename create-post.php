bab0985877663d968fbeb6a246a9a5dc9a9133d6
UrosRadin <neleiuki02@gmail.com>
2022-11-03 13:19:00 -0400

    Ajde radi

---
+++ create-post.php
@@ -0,0 +1,19 @@
+<main role="main" class="container">
+    <div class="row">
+            <?php 
+                include "header.php";
+                include "sidebar.php";
+            ?>  
+        <!-- ovde ce ici forma za kreiranje posta -->
+
+
+
+
+
+
+        
+    </div>
+</main>
+<?php
+    include "footer.php";
+?>
