b1070b48aa49843f55d41bbbd80968bf8b566717
UrosRadin <neleiuki02@gmail.com>
2022-11-04 12:35:53 -0400

    new updateee

--- posts.php
+++ posts.php
@@ -1,49 +1,38 @@
 <?php
     include ('db.php');
-    include ('comments.php');
 ?>
 <main role="main" class="container">
     <div class="row">
         <div class="col-sm-12 blog-main">
             <div class="blog-post">
-            <?php
-                $sql = "SELECT Id, Title, Body, Author, Created_at FROM posts ORDER BY Created_at DESC";
-                $statement = $connection->prepare($sql);
-                $statement->execute();
-                $statement->setFetchMode(PDO::FETCH_ASSOC);
-                $posts = $statement->fetchAll();
-            ?>
-
-            <?php foreach ($posts as $post) { ?>
-                <article class="blog-post">
-                    <header>
-                        <h1 class="blog-post-title">
-                            <a href="single_post.php?post_id=<?php echo($post['Id']) ?>">
-                            <?php echo($post['Title']); ?></a>
-                        </h1>
-                        <div class="blog-post-meta">
-                            <?php 
-                                echo($post['Created_at']);?> by 
-                                <?php echo($post['Author']);
-                            ?>
+                <?php
+                    $dbManagement = new DbManagement();
+                    $posts = $dbManagement->getAllPostsByDescOrder();
+                ?>
+                <?php foreach ($posts as $post) { ?>
+                    <article class="blog-post">
+                        <header>
+                            <h1 class="blog-post-title">
+                                <a href="single_post.php?post_id=<?php echo ($post['Id']) ?>">
+                                    <?php echo ($post['Title']); ?>
+                                </a>
+                            </h1>
+                            <div class="blog-post-meta">
+                                <?php echo ($post['Created_at']); ?> by <?php echo ($post['Author']);?>
+                            </div>
+                        </header>
+                        <div>
+                            <p class="blog-post">
+                                <?php echo ($post['Body']); ?>
+                            </p>
                         </div>
-                    </header>
-                    <div>
-                        <p class="blog-post"><?php echo($post['Body']); ?></p>
-                    </div>
-                </article>
-            <?php } ?>
-            <ul>
-            <?php $comments = $dbManagement->getCommentsByPostId($_GET['post_id']); ?>
-            <?php foreach ($comments as $comment) { ?>
-                <li>
-                    <h6><b><?php echo "$comment[Author]"; ?></b></h6>
-                    <h5><?php echo "$comment[Text]"; ?></h5>
-            </li>
-            <hr>
-            <?php } ?>
-            </ul>
+                    </article>
+                <?php } ?>
             </div>
+            <nav class="blog-pagination">
+                <a class="btn btn-outline-primary" href="#">Older</a>
+                <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
+            </nav>
         </div>
     </div>
 </main>
